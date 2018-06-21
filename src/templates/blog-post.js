9517e6882fa9da44b96bb6b8aafe8566c9f53e63
saigowthamr <saigowthamr@gmail.com>
2018-06-21 12:19:38 +0530

    errors fixed

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import createHistory from 'history/createBrowserHistory'
 import Helmet from 'react-helmet'
 
-export default ({ data }) => {
-  const post = data.markdownRemark
-  const location = window.location
+
+export default (props) => {
+  const post = props.data.markdownRemark
+  console.log(props)
+  console.log(props.match.path)
   return (
     <div>
       <Helmet
         title={post.frontmatter.title}
         meta={[{ name: 'description', content: post.frontmatter.description },
-          { property: "og:url", content: location.href},
+          { property: "og:url", content:props.location.pathname},
           { name: "twitter:card", content:"summary_large_image"} ,
           { property: "og:image", content: post.frontmatter.thumbnail && post.frontmatter.thumbnail}
         ]}
