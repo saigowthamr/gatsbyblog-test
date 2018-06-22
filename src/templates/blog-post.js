8e9426101c89225500d6a8e2f7abb221accf0502
saigowthamr <saigowthamr@gmail.com>
2018-06-22 10:36:41 +0530

    og tags all

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -22,17 +22,43 @@ export default props => {
               props.data.site.siteMetadata.url + post.frontmatter.thumbnail,
           },
           {
-            name: 'twitter:image',
-            content:
-              post.frontmatter.thumbnail &&
-              props.data.site.siteMetadata.url + post.frontmatter.thumbnail,
+            property: 'fb:app_id',
+            content: '277289552818368',
+          },
+          {
+            property: 'og:description',
+            content: post.frontmatter.description
+          },
+          {
+            name: 'twitter:description',
+            content: post.frontmatter.description
           },
           {
             name: 'twitter:image:src',
             content: post.frontmatter.thumbnail &&
               props.data.site.siteMetadata.url + post.frontmatter.thumbnail,
+
           },
+
+          {
+            rel: 'author',
+            href: 'https://twitter/@saigowthamr',
+          },
+          { property: 'author', content: 'Sai gowtham' },
+          { property: 'og:type', content: 'article' },
           { name: 'twitter:card', content: 'summary_large_image' },
+          {
+            property: 'article:publisher',
+            content: 'https://www.twitter.com/@saigowthamr',
+          },
+          { property: 'article:author', content: 'Sai gowtham' },
+          { name: 'robots', content: 'index, follow' },
+          {
+            property: 'article:published_time',
+            content: post.frontmatter.date,
+          },
+          { name: 'twitter:creator', content: '@saigowthamr' },
+          { property: 'og:site_name', content: 'saigowtham' },
         ]}
       />
       <h1>{post.frontmatter.title}</h1>
@@ -51,6 +77,7 @@ export const query = graphql`
         title
         description
         thumbnail
+        date(formatString: "DD MMMM, YYYY")
       }
     }
     site {
